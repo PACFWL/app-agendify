63043f47aa4686576416854c45da3dd8c41fb69f
PACFWL <estudanteprincipalgerali@gmail.com>
2025-03-22 18:52:55 -0300

    ✨ feat: "Adição de logo"

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthContext } from "../contexts/AuthContext";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen";
-import { View, ActivityIndicator } from "react-native";
+import SplashScreen from "../screens/SplashScreen";
+
 
 export type RootStackParamList = {
   Login: undefined;
@@ -17,12 +18,19 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const Routes = () => {
   const auth = useContext(AuthContext);
 
-  if (auth === null) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#6200ee" />
-      </View>
-    );
+  const [showSplash, setShowSplash] = useState(true); 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (auth === null || showSplash) {
+    
+    return <SplashScreen />;
   }
 
   return (
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, Button, Alert } from "react-native";
 import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/HomeScreenStyles"
 
 const HomeScreen = () => {
   const auth = useContext(AuthContext);
@@ -15,9 +16,11 @@ const HomeScreen = () => {
   };
 
   return (
-    <View>
-      <Text>Bem-vindo à Home!</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Bem-vindo à Home!</Text>
+      <View style={styles.button}>
       <Button title="Sair" onPress={handleSignOut} />
+      </View>
     </View>
   );
 };
